07a418181e7404e63ef65fb9bdd303fa2f96b736
CHARET Mohamed <92872392+medcharet@users.noreply.github.com>
2023-11-29 23:23:16 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -25,11 +25,19 @@ st.sidebar.markdown("- Location: Rabat, Maroc")
 resume_url = "CV_CHARET_MOHAMED.pdf"
 with open(resume_url, "rb") as file:
     btn = st.sidebar.download_button(
-        label="Download Resume",
+        label="Download Resume(version français)",
         data=file,
         file_name="CV_CHARET_MOHAMED.pdf",
         mime="application/pdf"
     )
+resume_url1 = "CV_ENGLISH.pdf"
+with open(resume_url1, "rb") as file:
+    btn = st.sidebar.download_button(
+        label="Download Resume(version anglais)",
+        data=file,
+        file_name="CV_ENGLISH.pdf",
+        mime="application/pdf"
+    )    
 st.sidebar.markdown("[LinkedIn](https://www.linkedin.com/in/mohamed-charet-952077223/)")
 
 # Education section
